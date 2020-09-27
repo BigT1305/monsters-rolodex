877954dfd3b89c6fbea19ba822b5fd94f7b0dcb5
Anthony Hoss <BigT1305@users.noreply.github.com>
2020-09-27 15:20:54 -0500

    Updates

---
+++ precache-manifest.d86dd807b8263bb7df41e9fceef6c147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e1f6f88f365108adf075db9bc74b52",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2f6e926ebaa17c5bf17d",
+    "url": "/monsters-rolodex/static/css/main.cb2ecf93.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6e926ebaa17c5bf17d",
+    "url": "/monsters-rolodex/static/js/main.4e8bd86b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
